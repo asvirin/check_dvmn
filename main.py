130e9f4830f75ec26a4ec749caee27b5df023404
asvirin <45921969+asvirin@users.noreply.github.com>
2019-05-18 20:05:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,39 +3,50 @@ import os
 import requests
 import time
 import telegram
-
-def get_status_homework():
+    
+def send_message(answer):
+    bot = telegram.Bot(token=os.getenv("telegram_token"))
+    chat_id = os.getenv("chat_id")
+    
+    lesson_title = answer['new_attempts'][0]['lesson_title']
+    lesson_url = 'https://dvmn.org{}'.format(answer['new_attempts'][0]['lesson_url'])
+    status_answer = answer['new_attempts'][0]['is_negative']
+    
+    if status_answer is True:
+        message = 'Задача "{}" проверена, но есть ошибки( \n*Ссылка*: {}'.format(lesson_title, lesson_url)
+    elif status_answer is False:
+        message = 'Задача проверена, ошибок нет!'
+    
+    bot.send_message(chat_id=chat_id, text=message, parse_mode=telegram.ParseMode.MARKDOWN)
+        
+    
+def get_status_homework(timestamp):
+    bot = telegram.Bot(token=os.getenv("telegram_token"))
+    chat_id = os.getenv("chat_id")
+    
     url = 'https://dvmn.org/api/long_polling/'
+    params = {'timestamp': timestamp}
+    headers = {'Authorization': os.getenv("authorization_token_dvmn")}
+    
     response = requests.get(url, headers=headers, params=params)
     answer = response.json()
     
-    if 'error' in response.text:
+    if response.ok is False:
         raise requests.exceptions.HTTPError(bot.send_message(chat_id=chat_id, \
-                                                             text='Ошибка при запросе: {}'.format(answer['error'])))
-    status_answer = answer['status']
-    
-    if status_answer == 'timeout':
-        timestamp = answer['timestamp_to_request']
-    elif status_answer == 'found' and answer['solution_attempts']['is_negative'] is True:
-        bot.send_message(chat_id=chat_id, text='Задача проверена, но есть ошибки(')
-    elif status_answer == 'found' and answer['solution_attempts']['is_negative'] is False:
-        bot.send_message(chat_id=chat_id, text='Задача проверена, ошибок нет!')
-    
+                                                             text='Ошибка при запросе: {}'.format(answer)))
+    elif answer['status'] == 'timeout':
+        return answer['timestamp_to_request']
+    else:
+        send_message(answer)
+        return answer['last_attempt_timestamp']
     
 if __name__ == '__main__':
-    telegram_token = os.getenv("telegram_token")
-    authorization_token_dvmn = os.getenv("authorization_token_dvmn")
-    chat_id = os.getenv("chat_id")
-    
-    headers = {'Authorization': authorization_token_dvmn}
-    params = {'timestamp': timestamp}
-    timestamp = int(time.time()) 
-    bot = telegram.Bot(token=telegram_token)
-        
+    timestamp = int(time.time())
     while True:
         try:
-            get_status_homework()
+            timestamp = get_status_homework(timestamp)
         except requests.exceptions.ReadTimeout:
-            get_status_homework()
+            timestamp = get_status_homework(timestamp)
         except requests.ConnectionError:
-            get_status_homework()
+            timestamp = get_status_homework(timestamp)
+    
