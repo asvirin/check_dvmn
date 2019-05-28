a99553acb9aa5b244da46e9d7e008cb70da836c8
asvirin <45921969+asvirin@users.noreply.github.com>
2019-05-28 23:41:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,18 +6,14 @@ import logging
 
 class MyLogsHandler(logging.Handler):
     def emit(self, record):
-        bot_error = telegram.Bot(token=os.environ['telegram_token'])
-        chat_id = os.environ['chat_id']
-        
+        bot_error = bot
         log_entry = self.format(record)
-        bot_error.send_message(chat_id=chat_id, text=log_entry)
+        bot_error.send_message(chat_id=chat_id, text='Сервисное сообщение от логгера ↓\n{}'.format(log_entry))    
 
-def send_message(message):
-    bot = telegram.Bot(token=os.environ['telegram_token'])
-    chat_id = os.environ['chat_id']
+def send_message(message, bot):
     bot.send_message(chat_id=chat_id, text=message, parse_mode=telegram.ParseMode.MARKDOWN, disable_web_page_preview=True)
     
-def get_text_answer(answer):
+def get_last_timestamp(answer, bot):
     if answer['status'] == 'timeout':
         return answer['timestamp_to_request']
     
@@ -26,40 +22,49 @@ def get_text_answer(answer):
     status_answer = answer['new_attempts'][0]['is_negative']
     
     if status_answer:
-        message = 'Задача "{}" проверена, но есть ошибки( \n*Ссылка*: {}'.format(lesson_title, lesson_url)
+        message = 'Задача "{}" проверена, но есть ошибки( \n[*Ссылка*]({})'.format(lesson_title, lesson_url)
     else:
         message = 'Задача проверена, ошибок нет!'
-    
-    send_message(message)
+
+    send_message(message, bot)
     return answer['last_attempt_timestamp']
     
-def get_status_homework(timestamp):
+def return_new_timestamp(timestamp, bot):
     url = 'https://dvmn.org/api/long_polling/'
     params = {'timestamp': timestamp}
-    headers = {'Authorization': os.environ['authorization_token_dvmn']}
+    headers = {'Authorization': dvmn_api_token}
     
     response = requests.get(url, headers=headers, params=params)
     answer = response.json()
-    
+        
     try:
         response.raise_for_status()
-        return get_text_answer(answer)
+        return get_last_timestamp(answer, bot)
     except requests.exceptions.HTTPError:
         message = 'Ошибка при запросе статуса задачи: {}'.format(answer)
-        send_message(message)
+        send_message(message, bot)
         time.sleep(3600)
     
 if __name__ == '__main__':
     timestamp = int(time.time())
+    
+    dvmn_api_token = os.environ['authorization_token_dvmn']
+    
+    telegram_token = os.environ['telegram_token']
+    chat_id = os.environ['chat_id']
+    bot = telegram.Bot(token=telegram_token)
+    
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
     logger.addHandler(MyLogsHandler())
+    
     logger.info("Бот запущен")
+    
     while True:
         try:
-            timestamp = get_status_homework(timestamp)
+            timestamp = return_new_timestamp(timestamp, bot)
         except (requests.exceptions.ReadTimeout, requests.ConnectionError):
             continue
         except Exception as err:
-            logger.info('Что-то сломалось, нужно чинить, вот ошибка ↓')
-            logger.info(err, exc_info=True)
+            logger.exception(err)
+            time.sleep(14400)
